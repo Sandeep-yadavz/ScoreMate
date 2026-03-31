5b9cc16dc492882e0ebc6b745197534d7c106f9a
Sandeep-yadavz <ravisandy00@gmail.com>
2026-03-31 23:56:48 +0530

    adding user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_player = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
